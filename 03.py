e684a5fd1c689618429e3f313c2a3e040dc796c9
表歌 <1301239018@qq.com>
2019-07-08 22:35:58 +0800

    Cloud Studio 连接Github成功！

---
+++ 03.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# utf-8
+
+# 分支结构   if elif 和 else
+
+print('11')
+
+""" 
+def inCtCm():
+    #英制单位英寸和公制单位厘米互换
+    value = float(input('请输入长度： '))
+    unit = input('请输入单位： ')
+
+    if unit == 'in' or unit == '英寸':
+        print('%f英寸 = %f厘米' % (value, value * 2.54))
+    elif unit == 'cm' or unit == '厘米':
+        print('%f厘米 = %f英寸' % (value, value % 2.54))
+    else:
+        print('请输入有效的单位')
+inCtCm() """
+
+""" def castDice():
+    # 根据骰子决定做什么
+    from random import randint
+
+    face = randint(1, 6)
+    if face == 1:
+        result = '唱歌'
+    elif face == 2:
+        result = '跳舞'
+    elif face == 3:
+        result = 'rap'
+    elif face == 4:
+        result = '篮球'
+    elif face == 5:
+        result = '打坤坤'
+    elif face == 6:
+        result = '江郎才尽了'
+    
+    print(result)
+
+
+castDice() """
+
