847071dff055e4763ee62b04ba8a81ae105abfe5
KevinBrother <1301239018@qq.com>
2019-07-10 17:33:53 +0800

    05还有个craps结尾

--- 05.py
+++ 05.py
@@ -1,128 +1,106 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#打印三角形图案
-def printTriangle():
-    num = int(input('打印几行的三角？'))
+#craps赌博游戏
+#两个骰子 第一次和为7或11 玩家胜，和为2、3、12玩家输
+# 如果是其他数字，记录第一次的和，再次投掷至和第一次的值一样，玩家胜，
+# 如果投掷出和为7，则玩家输
 
-    # for x in range(num):                   
-    #     for y in range(num):
-    #       if num - x - y - 1 > 0:
-    #         print(' ', end = '')
-    #       else:
-    #         print('*', end = '')
-    #     print()
-
-    # for x in range(1, row+1):
-    #     #print('这是第%i行', x)
-    #     for _ in range(0, x):
-    #         print('*', end='')
-    #     print()
-
-    for i in range(num):
-        for _ in range(num - i -1):
-            print(' ', end='')
-        for _ in range(2 * i + 1):
-            print('*', end='')
-        print()
-
-printTriangle()  
-
-""" 
-# 循环结构 for-in 和 while
-
-def maxAppAndMinMult():
-    num1 = int(input('输入第一个数字：'))
-    num2 = int(input('输入第二个数字：'))
-
-    maxApp = 1
-# 找出能被两个数整除的数，
-# 最大公约数是所有被整除的数的乘机
-# 最小公倍数是再乘以两个数剩余的部分
-
-    if num1 > num2:
-        num1, num2 = num2, num1    
-
-    for x in range(num1-1, 1, -1):
-        print(x)
-        if num1 % x == 0 and num2 % x == 0:
-            print('输出值啊 ')
-            num1 = num1 / x
-            num2 = num2 / x
-            maxApp = maxApp * x
-
-    litMult = maxApp * num1 * num2    
-    print('最大公约数： %i' % maxApp)
-    print('最小公倍数： %i' % litMult)
-    print('最小公倍数： %i' % (maxApp * num1 * num2))
-        
-maxAppAndMinMult() 
- """
-
-""" 
-# 输入一个数 是不是素数
-def isPrime():
-    num = int(input('请输数字： '))
-
-    if(num < 2):
-        print('请输入大于1的正整数!')
-
-    is_prime = True
-
-    for x in range(2, num - 1): 
-        if num % x == 0:
-            is_prime = False
-            break
-        else:
-            x += 1
-
-    if is_prime:
-        print('%i是素数！' % num)
-    else:
-        print('%i不是素数！' % num)
-
-isPrime() """
-
-    
-            
-    
-""" 
-# 猜一百以内的数
-def inHun():
-
+def craps():
     from random import randint
-    mun = randint(1, 100)
-    
-    count = 0
-    while True:
-        value = int(input('请输入100以内的数：'))
-        count += 1 
-        if value > mun:
-            print('小一点！')
-        elif value < mun:
-            print('大一点！')
-        else:
-            print('猜对啦！')
-            print('您一共猜了%d次！' % count)
-            break
+    one = randint(1, 7) 
+    two = randint(1, 7)
+    first = one + two
 
-    if count > 7:
-        print('您的智商余额不足，请充值！')
-    
-inHun()     """
+    if first == 7 or first == 11:
+       # print('玩家胜!')
+        return True
+    elif first == 2 or first == 3 or first == 12:
+       # print('玩家输!')
+        return False
+    else:
+        while True:
+            s_one = randint(1, 7)
+            s_two = randint(1, 7)
+            second = s_one + s_two
 
+            if first == second:
+                #print('玩家胜')
+                #break
+                return True
+            elif second == 7:
+                #print('玩家输')
+                #break
+                return False
+
+def countCraps():
+    count = 0 
+    for _ in range(1000):
+        if craps():
+            count += 1
+    print(count)
+countCraps()
+# craps()
+
+
+""" 
+#斐波那契数列
+# f(1) = 1, f(2) = 2, fn = f(n-1) + f(n-2)
+def fib():
+    pre = 0
+    next = 1
+    for num in range(10):
+        # fib = pre + next
+        # pre, next = next, fib
+        pre, next = next, pre + next
+        print(fib, end=' ')
+
+fib()
+ """
+
+
+""" 
+# 百钱百鸡  公鸡五钱，母鸡三钱， 三个小鸡一钱
+def hundred():
+    # 5x + 3y + z = 100
+    # x + y + 3z = 100
+
+    for x in range(20):
+        for y in range(33):
+            z = 100 - 5 * x - 3 * y
+            if x + y + z * 3 == 100:
+                print('公鸡：%i, 母鸡%i, 小鸡%i' % (x, y, 3 * z))
+            # for z in range (34):
+            #     if 5 * x + 3 * y + z == 100 and x + y + 3 * z == 100:
+            #         print('公鸡：%i, 母鸡%i, 小鸡%i' % (x, y, 3 * z))
+
+hundred()
+ """
 
 
 
 """ 
-# 一百以内的偶数相加    
-def evenAdd():
-    sum = 0
-    for x in range(2, 101, 2):
-        sum += x
-    
-    print(sum)
+# 水仙花数。
+def lily():
+    for num in range(100, 1000):
+        low = num % 10
+        mid = num // 10 % 10 
+        big = num // 100
 
-evenAdd()
+        if num == low ** 3 + mid **3 + big ** 3:
+            print(num) 
 
- """
+lily() """
+""" 
+# 完美数
+def perfect():
+    for num in range(1, 10000):
+        count = 0   
+        for i in range(1, num):
+            if num % i == 0:
+                count += i
+        
+        if num == count:
+            print(num)
+
+perfect() """
