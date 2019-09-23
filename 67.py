2338bca556f523719fa726b22c41bd2b3a15b134
wwwyugen12345 <1301239018@qq.com>
2019-09-23 22:21:42 +0800

    知乎爬虫

---
+++ 67.py
@@ -0,0 +1,37 @@
+# 数据采集
+# pip install bs4
+# pip install lxml
+
+from urllib.parse import urljoin
+
+import re
+import requests
+
+from bs4 import BeautifulSoup
+
+
+def main():
+    headers = {'user-agent': 'Baiduspider'}
+    proxies = {
+        'http': 'http://122.114.31.177:808'
+    }
+    base_url = 'https://www.zhihu.com/'
+    seed_url = urljoin(base_url, 'explore')
+    print(seed_url)
+    resp = requests.get(seed_url,
+                        headers=headers,
+                        proxies=proxies)
+    soup = BeautifulSoup(resp.text, 'lxml')
+    href_regex = re.compile(r'^/question')
+    link_set = set()
+    for a_tag in soup.find_all('a', {'href': href_regex}):
+        if 'href' in a_tag.attrs:
+            href = a_tag.attrs['href']
+            full_url = urljoin(base_url, href)
+            link_set.add(full_url)
+    print('Total %d question pages found.' % len(link_set))
+    print(link_set)
+
+
+if __name__ == '__main__':
+    main()
